6e8114222399024a98ac39603a3467b48ba38be7
shadowtime2000 <66655515+shadowtime2000@users.noreply.github.com>
2020-10-15 19:09:25 -0700

    feat: prettier as devdep

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,23 @@
 module.exports = {
-	env: {
-		browser: false,
-		node: true,
-		mocha: true,
-		es6: true
-	},
-	parserOptions: {
-		ecmaVersion: 12
-	},
-	globals: {
-		__dirname: true
-	},
-	extends: "eslint:recommended",
-	rules: {
-		quotes: [
-			"error",
-			"double"
-		],
-		"prefer-const": "error",
-		"no-const-assign": "error",
-		"no-var": "error",
-		"no-new-object": "error",
-		"prefer-arrow-callback": "error"
-	}
+  env: {
+    browser: false,
+    node: true,
+    mocha: true,
+    es6: true,
+  },
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  globals: {
+    __dirname: true,
+  },
+  extends: "eslint:recommended",
+  rules: {
+    quotes: ["error", "double"],
+    "prefer-const": "error",
+    "no-const-assign": "error",
+    "no-var": "error",
+    "no-new-object": "error",
+    "prefer-arrow-callback": "error",
+  },
 };
